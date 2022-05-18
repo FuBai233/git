560a39e57ceb39ec8ae3b9f49cb8186f08abc633
lmy <limengyang33233@163.com>
2022-05-19 02:19:17 +0800

    hot-fix commit1

--- src/main/java/com/kgc/GitTest.java
+++ src/main/java/com/kgc/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
         System.out.println("hello.git2");
         System.out.println("hello.git2");
         System.out.println("hello.git2");
+        System.out.println("hello.git444444444444444444");
 
     }
 }
